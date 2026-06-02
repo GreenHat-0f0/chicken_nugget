cbfc3769d84559217a055140fbfb444f4ecf83d3
Your Name <you@example.com>
2026-06-02 17:08:35 -0300

    rthjtrgfk

---
+++ feet/ativ7.py
@@ -0,0 +1,9 @@
+x=int(input("Digite seu mês de nascimento: "))
+y=int(input("Digite seu dia de nascimento: "))
+print("""
+        ------
+       /(o)(o)|
+      | ,___,  |
+       |      |
+        |    |   
+                    """)
