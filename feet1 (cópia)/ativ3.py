8abfc958010223d7ac3a3d13277eeab36d151b6f
00ff00-green-hat <kiara.paasrosa@gmail.com>
2026-06-10 13:42:10 -0300

    Delete feet1 (cópia) directory

--- feet1 (cópia)/ativ3.py
+++
@@ -1,6 +0,0 @@
-us = (input("Digite seu usuário: "))
-pas = int(input("Digite sua senha: "))
-if us == "admin" and pas == 12345:
-    print("Login bem-sucedido")
-else:
-    print("Nome de usuário ou senha incorretos")
