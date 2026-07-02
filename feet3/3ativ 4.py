eb82c31b1085356d529e66ca7408a0baf4bdeaab
GreenHat-0f0 <kiara.paasrosa@gmail.com>
2026-07-02 16:08:43 -0300

    sdvfbffgwsdg

--- feet3/3ativ 4.py
+++ feet3/3ativ 4.py
@@ -9,8 +9,49 @@
 # houver, deve proceder o cadastro. Se não, deve informar o usuário que não há espaço
 # disponível. A opção Excluir deve perguntar ao usuário qual placa deve ser excluída (pelo
 # nome da placa) e informar se houve sucesso ou falha. Já a opção listar deve
-# Instituto Federal de Santa Catarina – Reitoria
-# Rua: 14 de julho, 150 | Coqueiros | Florianópolis /SC | CEP: 88.075-010
-# Fone: (48) 3877-9000 | www.ifsc.edu.br | CNPJ 11.402.887/0001-60
 # simplesmente listar todas as placas cadastradas. Dica: utilize um valor padrão para definir
 # um espaço vago na lista.
+
+import sys
+n = 0
+pdts = [-1]*15
+def grah():
+    global pdts, n
+    print("""
+    Menu
+    ----
+    1 - Cadastrar
+    2 - Listar todos
+    3 - Exluir
+    0 - Sair 
+        
+        """)
+
+
+    x = input("Digite uma opção: ")
+    if x == 1:
+        def AAA():
+            global pdts, n
+            cadastro = input("Digite o placa do carro que você deseja cadastrar: ")
+            if cadastro != -1:
+                pdts[n] = cadastro
+                n += 1
+            else:
+                print("ISTO NAO E UMA OPCAO VALIDA, TOLO!!!")
+                AAA()
+            return n, pdts
+        if n<15:
+            n, pdts = AAA()
+        else:
+            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            print("O estacionamento esta cheio.")
+        grah()
+    elif x == 2:
+        for i in pdts:
+            if i != -1:
+                print(i)
+        grah()
+    elif x == 3:
+        lixo = input("Qual a placa do carro que voce deseja exluir? ")
+    return x, pdts, n
+x, pdts, n = grah()
